7a7473b4839fa889629d2066924fb86da544a1b6
Youssef Raad <134856706+YoussefRaad-mathecon@users.noreply.github.com>
2024-05-17 21:40:59 +0200

    Update 2.2.handin3.py

--- 2.2.handin3.py
+++ 2.2.handin3.py
@@ -1,6 +1,7 @@
 import numpy as np
 import time
-np.random.seed(100)
+import matplotlib.pyplot as plt
+np.random.seed(666)
 
 
 S0 = 100
@@ -16,7 +17,7 @@ n = 100
 N = 1000
 K = 100
 
-
+# See slides CTF 2: stochastic volatility models and Fourier methods April 2, 2024 45 and Euler and Milstein Discretization slide 4
 def generateHestonPathMilsteinDisc(S0, v0, r, kappa, theta, sigma, rho, lambda_, T, n):
     kappa_tilde = kappa + lambda_
     theta_tilde = (kappa*theta)/(kappa+lambda_)
@@ -35,9 +36,9 @@ def generateHestonPathMilsteinDisc(S0, v0, r, kappa, theta, sigma, rho, lambda_,
         
         dv =  kappa_tilde * (theta_tilde - v[i-1]) * dt + sigma * np.sqrt(v[i-1] * dt) * Zv + 0.25 * sigma**2 * dt * (Zv**2 - 1) 
         v[i] = v[i-1] + dv
-        if v[i] <= 0:
+        if v[i] < 0:
             v_zero_count += 1
-            v[i] = 0 
+            v[i] = 0 # Applying full truncation
 
         dS = r * S[i-1] * dt + np.sqrt(max(v[i-1], 0) * dt) * S[i-1] * Zs 
         S[i] = S[i-1] + dS
@@ -57,10 +58,38 @@ def priceHestonCallViaMilsteinMC(S0, v0, r, kappa, theta, sigma, rho, lambda_, T
     end_time = time.time()
     computing_time = end_time - start_time
     
-    return option_price, std_dev, computing_time, v_zero_count
+    return option_price, std_dev, computing_time, v_zero_count, payoffs
 
-option_price, std_dev, computing_time, v_zero_count = (priceHestonCallViaMilsteinMC(S0, v0, r, kappa, theta, sigma, rho, lambda_, T, n, N, K))
+option_price, std_dev, computing_time, v_zero_count, payoffs = (priceHestonCallViaMilsteinMC(S0, v0, r, kappa, theta, sigma, rho, lambda_, T, n, N, K))
 print(f"The option price is (MilsteinMC): {option_price}")
 print(f"The standard deviation of the option price is (MilsteinMC): {std_dev}")
 print(f"The computing time is (MilsteinMC): {computing_time} seconds")
 print(f"The count of zero variance occurences is (MilsteinMC): {v_zero_count}")
+
+num_paths = N
+paths = []
+for i in range(num_paths):
+    S, _ = generateHestonPathMilsteinDisc(S0, v0, r, kappa, theta, sigma, rho, lambda_, T, n)
+    paths.append(S)
+
+
+# Plotting the stock paths
+plt.figure(figsize=(14, 6))
+for S in paths:
+    plt.plot(S)
+plt.title('Sample Stock Price Paths under the Heston Model')
+plt.xlim(0, 100)
+plt.xlabel('Time Steps')
+plt.ylabel('Stock Prices')
+plt.grid(True)
+plt.show()
+
+
+# Plot the option prices
+plt.figure(figsize=(14, 6))
+plt.plot(payoffs, marker='o', linestyle='-', color='blue')
+plt.title('Payoffs under the Heston Model')
+plt.xlabel('Payoff number')
+plt.ylabel('Payoffs')
+plt.grid(True)
+plt.show()
